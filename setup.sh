935026136f95ff8884eca0f0a0966516eaa23b25
DanStough <daniel.stough@gmail.com>
2020-08-09 22:45:47 +0000

    Fix script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 yum update -y
 yum install -y vim git bash-completion
-git clone https://github.com/sandervanvugt/cka.git
+git clone https://github.com/DanStough/cka.git 
 cd cka
 source ./setup-docker.sh
 source ./setup-kubetools.sh
